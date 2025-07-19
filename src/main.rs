fd61cc6bde289e6e4a26b4204b072beb11f0e751
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-07-19 21:34:21 +0100

    no longer loop over all pixels to draw box

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#![deny(clippy::all)]
-#![forbid(unsafe_code)]
+use std::mem::{transmute, transmute_copy};
 
 use error_iter::ErrorIter as _;
 use log::error;
@@ -17,124 +16,152 @@ const BOX_SIZE: i16 = 64;
 
 /// Representation of the application state. In this example, a box will bounce around the screen.
 struct World {
-    box_x: i16,
-    box_y: i16,
-    velocity_x: i16,
-    velocity_y: i16,
+	box_x: i16,
+	box_y: i16,
+	velocity_x: i16,
+	velocity_y: i16,
 }
 
 fn main() -> Result<(), Error> {
-    env_logger::init();
-    let event_loop = EventLoop::new().unwrap();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new()
-            .with_title("Hello Pixels")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
+	env_logger::init();
+	let event_loop = EventLoop::new().unwrap();
+	let mut input = WinitInputHelper::new();
+	let window = {
+		let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+		WindowBuilder::new()
+			.with_title("Hello Pixels")
+			.with_inner_size(size)
+			.with_min_inner_size(size)
+			.build(&event_loop)
+			.unwrap()
+	};
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
-    let mut world = World::new();
+	let mut pixels = {
+		let window_size = window.inner_size();
+		let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+		Pixels::new(WIDTH, HEIGHT, surface_texture)?
+	};
+	let mut world = World::new();
 
-    let res = event_loop.run(|event, elwt| {
-        // Draw the current frame
-        if let Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } = event
-        {
-            world.draw(pixels.frame_mut());
-            if let Err(err) = pixels.render() {
-                log_error("pixels.render", err);
-                elwt.exit();
-                return;
-            }
-        }
+	let res = event_loop.run(|event, elwt| {
+		// Draw the current frame
+		if let Event::WindowEvent {
+			event: WindowEvent::RedrawRequested,
+			..
+		} = event
+		{
+			world.draw(pixels.frame_mut());
+			if let Err(err) = pixels.render() {
+				log_error("pixels.render", err);
+				elwt.exit();
+				return;
+			}
+		}
 
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
-                elwt.exit();
-                return;
-            }
+		// Handle input events
+		if input.update(&event) {
+			// Close events
+			if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+				elwt.exit();
+				return;
+			}
 
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    log_error("pixels.resize_surface", err);
-                    elwt.exit();
-                    return;
-                }
-            }
+			// Resize the window
+			if let Some(size) = input.window_resized() {
+				if let Err(err) = pixels.resize_surface(size.width, size.height) {
+					log_error("pixels.resize_surface", err);
+					elwt.exit();
+					return;
+				}
+			}
 
-            // Update internal state and request a redraw
-            world.update();
-            window.request_redraw();
-        }
-    });
-    res.map_err(|e| Error::UserDefined(Box::new(e)))
+			// Update internal state and request a redraw
+			world.update();
+			window.request_redraw();
+		}
+	});
+	res.map_err(|e| Error::UserDefined(Box::new(e)))
 }
 
 fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
-    error!("{method_name}() failed: {err}");
-    for source in err.sources().skip(1) {
-        error!("  Caused by: {source}");
-    }
+	error!("{method_name}() failed: {err}");
+	for source in err.sources().skip(1) {
+		error!("  Caused by: {source}");
+	}
+}
+#[repr(C)]
+#[derive(Clone, Debug)]
+struct Colour {
+	red: u8,
+	green: u8,
+	blue: u8,
+	alpha: u8,
+}
+impl Colour {
+	pub fn new(red: u8, green: u8, blue: u8, alpha: u8) -> Self {
+		Self {
+			red,
+			green,
+			blue,
+			alpha,
+		}
+	}
+}
+impl Default for Colour {
+	fn default() -> Self {
+		Self::new(255, 255, 255, 255)
+	}
 }
 
 impl World {
-    /// Create a new `World` instance that can draw a moving box.
-    fn new() -> Self {
-        Self {
-            box_x: 24,
-            box_y: 16,
-            velocity_x: 1,
-            velocity_y: 1,
-        }
-    }
+	/// Create a new `World` instance that can draw a moving box.
+	fn new() -> Self {
+		Self {
+			box_x: 24,
+			box_y: 16,
+			velocity_x: 1,
+			velocity_y: 1,
+		}
+	}
 
-    /// Update the `World` internal state; bounce the box around the screen.
-    fn update(&mut self) {
-        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
-            self.velocity_x *= -1;
-        }
-        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
-            self.velocity_y *= -1;
-        }
+	/// Update the `World` internal state; bounce the box around the screen.
+	fn update(&mut self) {
+		if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
+			self.velocity_x *= -1;
+		}
+		if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
+			self.velocity_y *= -1;
+		}
 
-        self.box_x += self.velocity_x;
-        self.box_y += self.velocity_y;
-    }
+		self.box_x += self.velocity_x;
+		self.box_y += self.velocity_y;
+	}
 
-    /// Draw the `World` state to the frame buffer.
-    ///
-    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
-    fn draw(&self, frame: &mut [u8]) {
-        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let x = (i % WIDTH as usize) as i16;
-            let y = (i / WIDTH as usize) as i16;
+	/// Draw the `World` state to the frame buffer.
+	///
+	/// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
+	fn draw(&self, frame: &mut [u8]) {
+		let frame = frame_pixels(frame);
+		dbg!(
+			frame
+				.as_flattened_mut()
+				.fill(Colour::new(0x48, 0xb2, 0xe8, 255))
+		);
 
-            let inside_the_box = x >= self.box_x
-                && x < self.box_x + BOX_SIZE
-                && y >= self.box_y
-                && y < self.box_y + BOX_SIZE;
+		for row in &mut frame[(self.box_y as usize)..((self.box_y + BOX_SIZE) as usize)] {
+			row[(self.box_x as usize)..((self.box_x + BOX_SIZE) as usize)]
+				.fill(Colour::new(0x5e, 0x48, 0xe8, 255));
+		}
+	}
+}
 
-            let rgba = if inside_the_box {
-                [0x5e, 0x48, 0xe8, 0xff]
-            } else {
-                [0x48, 0xb2, 0xe8, 0xff]
-            };
-
-            pixel.copy_from_slice(&rgba);
-        }
-    }
+const fn frame_pixels(frame: &mut [u8]) -> &mut [[Colour; WIDTH as usize]] {
+	// SAFETY: Format for each pixel matches the layout of the `Colour` struct (and is 4 bytes)
+	// mem::transmute doesn't work here as it doesn't adjust the length of the slice, even though it is transmuted into a 2D array (so the length reduces)
+	let frame = unsafe {
+		let ptr = frame as *mut [u8];
+		let casted = ptr as *mut [[Colour; WIDTH as usize]; HEIGHT as usize];
+		&mut *casted
+	};
+	frame
 }
