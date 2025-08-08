cc0a6772a25400a053fa9f8fc72845e90fa3da66
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-08-08 15:25:21 +0100

    perf: optimize barycentric coordinate computation so there is less floating point arithmetic

--- crates/renderer/src/graphics/shapes_2d/triangle.rs
+++ crates/renderer/src/graphics/shapes_2d/triangle.rs
@@ -4,7 +4,7 @@ use crate::graphics::{
 	colour::Colour,
 	draw::Draw,
 	screen::Screen,
-	shapes_2d::{bounding_area::BoundingArea2D, line::Line, point::AbsoluteScreenCoordinate},
+	shapes_2d::{bounding_area::BoundingArea2D, point::AbsoluteScreenCoordinate},
 	viewport::Viewport,
 };
 
@@ -26,16 +26,17 @@ impl Triangle2D {
 			vertex3,
 		}
 	}
-	pub fn doubled_area(&self) -> usize {
+	pub fn signed_doubled_area(&self) -> i32 {
 		let (x1, y1, _) = self.vertex1.as_tuple();
 		let (x2, y2, _) = self.vertex2.as_tuple();
 		let (x3, y3, _) = self.vertex3.as_tuple();
 
-		i32::abs(
-			x1 as i32 * (y2 as i32 - y3 as i32)
-				+ x2 as i32 * (y3 as i32 - y1 as i32)
-				+ x3 as i32 * (y1 as i32 - y2 as i32),
-		) as usize
+		x1 as i32 * (y2 as i32 - y3 as i32)
+			+ x2 as i32 * (y3 as i32 - y1 as i32)
+			+ x3 as i32 * (y1 as i32 - y2 as i32)
+	}
+	pub fn doubled_area(&self) -> usize {
+		i32::abs(self.signed_doubled_area()) as usize
 	}
 	fn bounding_area(&self) -> BoundingArea2D {
 		let min_x = self.vertex1.x.min(self.vertex2.x).min(self.vertex3.x);
@@ -51,8 +52,8 @@ impl Triangle2D {
 	}
 }
 pub static mut TRIANGLE_RENDER_COUNT: usize = 0;
-fn absolute_screen_coordinate_to_2d_vec(p: AbsoluteScreenCoordinate) -> Vector2<f32> {
-	Vector2::new(p.x as f32, p.y as f32)
+fn absolute_screen_coordinate_to_2d_vec(p: AbsoluteScreenCoordinate) -> Vector2<i16> {
+	Vector2::new(p.x as i16, p.y as i16)
 }
 fn is_between_0_and_1(x: f32) -> bool {
 	x >= 0.0 && x <= 1.0
@@ -69,12 +70,14 @@ impl Draw for Triangle2D {
 		}
 		// println!("2");
 		// If all vertices are below the current pixels in the Z buffer, also don't draw
-		if viewport.point_below_z_buffer(screen, self.vertex1)
-			&& viewport.point_below_z_buffer(screen, self.vertex2)
-			&& viewport.point_below_z_buffer(screen, self.vertex3)
-		{
-			return;
-		}
+		// Can't do this optimisation as some triangles may still be visible, even with 
+		// all vertices below the Z buffer
+		// if viewport.point_below_z_buffer(screen, self.vertex1)
+		// 	&& viewport.point_below_z_buffer(screen, self.vertex2)
+		// 	&& viewport.point_below_z_buffer(screen, self.vertex3)
+		// {
+		// 	return;
+		// }
 		// println!("3");
 		// viewport.draw_line(screen, self.vertex1, self.vertex2);
 		// viewport.draw_line(screen, self.vertex2, self.vertex3);
@@ -86,22 +89,22 @@ impl Draw for Triangle2D {
 		// println!("4");
 		// Now need to fill in the triangle
 		let bounding_area = self.bounding_area();
-		// let abc = self.doubled_area();
-		// if abc == 0 {
-		// 	return;
-		// }
+		let abc = self.signed_doubled_area();
+		if abc == 0 {
+			return;
+		}
 		let v0 = absolute_screen_coordinate_to_2d_vec(self.vertex1);
 		let v1 = absolute_screen_coordinate_to_2d_vec(self.vertex2);
 		let v2 = absolute_screen_coordinate_to_2d_vec(self.vertex3);
-		let mat = Matrix2::new(v1 - v0, v2 - v0).inverse();
+		let mat = Matrix2::new(v1 - v0, v2 - v0).adjugate();
+		let denom = abc as f32;
 		// Iterate over all pixels that could possibly contain the triangle
 		for y in bounding_area.min_y..=bounding_area.max_y {
 			for x in bounding_area.min_x..=bounding_area.max_x {
-				let Vector2 { x: l0, y: l1 } = mat * (Vector2::new(x as f32, y as f32) - v0);
+				let Vector2 { x: l0, y: l1 } = mat * (Vector2::new(x as i16, y as i16) - v0);
+				let l0 = l0 as f32 / denom;
+				let l1 = l1 as f32 / denom;
 				let l2 = 1.0 - l0 - l1;
-				// let abp = Triangle2D::new(self.vertex1, self.vertex2, p).doubled_area();
-				// let bcp = Triangle2D::new(self.vertex2, self.vertex3, p).doubled_area();
-				// let acp = Triangle2D::new(self.vertex1, self.vertex3, p).doubled_area();
 
 				if is_between_0_and_1(l0) && is_between_0_and_1(l1) && is_between_0_and_1(l2) {
 					let z = self.vertex1.z * l0 + self.vertex2.z * l1 + self.vertex3.z * l2;
