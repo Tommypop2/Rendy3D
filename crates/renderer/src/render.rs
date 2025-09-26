d0fc29bb372603f81e902e4c2a3300309ab0dcd4
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-09-26 14:44:03 +0100

    refactor: massively simplify code for rendering a triangle

--- crates/renderer/src/render.rs
+++ crates/renderer/src/render.rs
@@ -31,57 +31,30 @@ pub fn render<M, P, T, U, V, F>(
 {
 	for triangle in mesh {
 		let transformed = triangle.apply(transform.clone());
-		// let projected = transformed.clone().apply(projection.clone());
-		// transformed
-		// 	.map_vertices(|vertex| {
-		// 		let vsout = pipeline.vertex(0, vertex);
-		// 		(
-		// 			vsout
-		// 				.0
-		// 				.apply(projection.clone())
-		// 				.to_pixel_coordinate(target.area()),
-		// 			vsout.1,
-		// 		)
-		// 	})
-		// 	.draw(target, pipeline);
-		Triangle::new(
-			{
-				let vsout = pipeline.vertex(0, transformed.vertex1);
-				let clip_space = projection.clone() * vsout.0.to_homogenous();
-				if !test_point(clip_space.clone()) {
-					continue;
-				}
+
+		// Convert triangle to clip space
+		let clip_space = transformed.map_vertices(|vertex| {
+			let vsout = pipeline.vertex(0, vertex);
+			let clip_space = projection.clone() * vsout.0.to_homogenous();
+			(clip_space, vsout.1)
+		});
+		// Test each vertex for clipping
+		if !(test_point(clip_space.vertex1.0)
+			&& test_point(clip_space.vertex2.0)
+			&& test_point(clip_space.vertex3.0))
+		{
+			// Clip!
+			continue;
+		}
+		// Convert to screen space
+		clip_space
+			.map_vertices(|(p, a)| {
 				(
-					Point::from_vector(Vector3::from_homogenous(clip_space))
+					Point::from_vector(Vector3::from_homogenous(p))
 						.to_pixel_coordinate(target.area()),
-					vsout.1,
+					a,
 				)
-			},
-			{
-				let vsout = pipeline.vertex(0, transformed.vertex2);
-				let clip_space = projection.clone() * vsout.0.to_homogenous();
-				if !test_point(clip_space.clone()) {
-					continue;
-				}
-				(
-					Point::from_vector(Vector3::from_homogenous(clip_space))
-						.to_pixel_coordinate(target.area()),
-					vsout.1,
-				)
-			},
-			{
-				let vsout = pipeline.vertex(0, transformed.vertex3);
-				let clip_space = projection.clone() * vsout.0.to_homogenous();
-				if !test_point(clip_space.clone()) {
-					continue;
-				}
-				(
-					Point::from_vector(Vector3::from_homogenous(clip_space))
-						.to_pixel_coordinate(target.area()),
-					vsout.1,
-				)
-			},
-		)
-		.draw(target, pipeline);
+			})
+			.draw(target, pipeline);
 	}
 }
